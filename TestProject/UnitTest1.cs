920ec734ae3991b3530d890931f6e3f3d86f169f
Alberto González Avendaño <albertoga03@gmail.com>
2024-10-25 11:49:55 -0600

    Test case con matriz

--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -1,18 +1,30 @@
 using Problemas;
+using System.Collections;
 
 namespace TestProject
 {
     public class Tests
     {
+
         [SetUp]
         public void Setup()
         {
         }
 
-        [TestCase(0,0)]
-        public void Test1(int n, int output)
+        public static IEnumerable TestCases
         {
-            Assert.AreEqual(ClaseProblemas.problema1(n), output);
+            get
+            {
+                yield return new TestCaseData(new int[][] { new int[] { 1, 2, 3 } }, 0);
+                yield return new TestCaseData(new int[][] { new int[] { 1, 5, 6 } }, 1);
+                // Add more test cases as needed
+            }
+        }
+
+        [Test, TestCaseSource(nameof(TestCases))]
+        public void Test1(int[][] n, int output)
+        {
+            Assert.AreEqual(ClaseProblemas.problema1(n[0][0]), output);
         }
     }
 }
